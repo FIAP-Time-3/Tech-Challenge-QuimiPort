d0e8316720a4e433e982d5109e1f13855584334b
Bruno Dias <bruno_dias@adata.com>
2026-09-10 13:02:28 -0300

    test: husky precommit

--- backend/src/modules/produtoQuimico/application/ProdutoQuimico.service.ts
+++ backend/src/modules/produtoQuimico/application/ProdutoQuimico.service.ts
@@ -14,9 +14,7 @@ export class ProdutoQuimicoService {
     body: CreateOrUpdateProdutoQuimicoDto;
   }): Promise<ProdutoQuimicoResponseDto> {
     const newProduct = await this.prisma.produtosQuimicos.create({
-
       data: { produto: body.name },
-      
     });
 
     return await this.findOne({ id: newProduct.id });
