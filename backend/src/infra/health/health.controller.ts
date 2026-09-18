83d8765c8503a71df49d6b6ceac8ccf737673d89
Bruno Dias <bruno_dias@adata.com>
2026-09-18 16:04:34 -0300

    feat: included health checks on dockerized services

--- backend/src/infra/health/health.controller.ts
+++ backend/src/infra/health/health.controller.ts
@@ -1,10 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthService } from './health.service.js';
-import { AllowedRoles } from '../security/decorators/allowedRoles.decorator.js';
-import { RolesEnum } from '../security/enums/roles.enum.js';
+import { SetPublic } from '../security/decorators/setPublic.decorator.js';
 
 @Controller('health')
-@AllowedRoles([RolesEnum.ADMINISTRADOR])
+@SetPublic()
 export class HealthController {
   constructor(private readonly service: HealthService) {}
 
